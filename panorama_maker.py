f288402d4a944e0630d4b03392aa2a192f14b5a6
Hila Manor <hila8manor@gmail.com>
2020-04-29 18:31:53 +0300

    Panorama maker structure

---
+++ panorama_maker.py
@@ -0,0 +1,32 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#
+#
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Imports
+
+
+# Constants
+
+
+
+class PanoramaMaker(Object):
+    def __init__(self):
+        pass
+
+    def add_photo(self, a, b):
+        """ blah balh ablhabla alhablah
+
+        :param self:
+        :param a: age
+        :param b: birthday
+
+        :return: blah balh
+        """
+        pass
+
+    def create_panorama(self):
+
+        return panorama_img
