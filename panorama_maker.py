04ac8f9129384903c630493c498842b220717886
adir <adirkk@gmail.com>
2020-05-08 11:37:29 +0300

    panorama_maker.py -> (in progress 3)

--- panorama_maker.py
+++ panorama_maker.py
@@ -23,15 +23,15 @@ class DescriptorType(Enum):
     SIFT = 4
     AKAZE = 5
 
-
 class PanoramaMaker:
-    def __init__(self, descriptor_type=DescriptorType.ORB):
+    def __init__(self, descriptor_type=DescriptorType.ORB, matcher_type=BF):
         self.__counter = 0 # todo do I need that counter? maybe for later use
         self.__photos = [] # empty list named photos
         self.__featuresKeys = [] # empty list named features
         self.__check_first = 0
-        self.__descriptor_type = descriptor_type
-        self.__descriptor_func = self.__choose_descriptor_type()
+        self.__descriptor_type = descriptor_type # todo - check: do I need here __ (as all - matcher)
+        self.__descriptor_func = self.__choose_descriptor_type() # used in detectAndDescribe
+        self.__matcher_type = matcher_type
 
     def __choose_descriptor_type(self):
         if self.__descriptor_type == DescriptorType.ORB:
@@ -68,28 +68,46 @@ class PanoramaMaker:
         # read image
         self.__photos.append(image)
         image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) #now our photo in gray for data extraction
-        key_points, photo_features = self.__detectAndDescribe(image_gray)
-        self.__featuresKeys.append(key_points, photo_features) # adding the photo information to the list as a tuple
-        if self.__check_first < len(photo_features):
+        key_points, photo_features = self.__detectAndDescribe(image_gray) # getting information
+        self.__featuresKeys.append(key_points, photo_features) # adding the photo's information to the list as a tuple
+        if self.__check_first < len(photo_features): # for later use - who's will be first
             self.__check_first = self.__counter
         self.__counter += 1  # todo - again, do I need that counter?
         
         # once you have all images processing
         def create_panorama(self):
+
+            i = 0
+            checkArray = [1] * self.__counter # which photos are already included - will change to zero inside the loop
+            while ( i < self.__counter ):
             # panorama =
 
+            # it's gonna save each time the new photo until it's gonna be panoramic
+
+
+            i += 1
             # search
+
+
+
 
             # return panorama
             pass
-
-
 
     def __detectAndDescribe(self, image):
         # Compute key points and feature descriptors using an specific method
         descriptor = self.__descriptor_func()
         # get keypoints and descriptors
         return descriptor.detectAndCompute(image, None)
+
+    def __matcherType(self): # the importance is the default section (BF)
+        if self.__matcher_type == BF
+            return BF
+        elif self.__matcher_type == KNN
+                return KNN
+        else:
+                print("no such matcher, choosing BF")
+                return BF
 
     def __createMatcher(self, crossCheck):
         # creates and returns a matcher object
@@ -105,22 +123,80 @@ class PanoramaMaker:
             raise("should not get here")  # We dealt with that previously, ORB is defaulty
         return bf
 
+    def __matchKeyPoints(self): # todo - don't forget make it robust
+
+        matcher = self.__matcherType()
+        if matcher == BF:
+            bf = createMatcher(method, crossCheck=True)
+        elif matcher == KNN:
+            bf = createMatcher(method, crossCheck=False)
+        else:
+            raise ("should not get here")  # We dealt with that previously, BF is defaulty
+
+        i = 0
+        best_matches = []
+        while i < self.__counter: # todo I'll need here that array to know who's to continue look for
+            if macther == bf:
+            best_matches[i] = bf.match(self.__featuresKeys[i][1], self.__featuresKeys[???][1] )
+
+
+
+
+        pass
+
+
+
+    def matchKeyPointsBF(featuresA, featuresB, method): # it's theirs!!!
+        bf = createMatcher(method, crossCheck=True)
+
+        # Match descriptors.
+        best_matches = bf.match(featuresA, featuresB)
+
+        # Sort the features in order of distance.
+        # The points with small distance (more similarity) are ordered first in the vector
+        rawMatches = sorted(best_matches, key=lambda x: x.distance)
+        print("Raw matches (Brute force):", len(rawMatches))
+        return rawMatches
+
+    def matchKeyPointsKNN(featuresA, featuresB, ratio, method): # it's theirs!
+        bf = createMatcher(method, crossCheck=False)
+        # compute the raw matches and initialize the list of actual matches
+        rawMatches = bf.knnMatch(featuresA, featuresB, 2)
+        print("Raw matches (knn):", len(rawMatches))
+        matches = []
+
+        # loop over the raw matches
+        for m, n in rawMatches:
+            # ensure the distance is within a certain ratio of each
+            # other (i.e. Lowe's ratio test)
+            if m.distance < n.distance * ratio:
+                matches.append(m)
+        return matches
 
 
 
 
 
-    def __matchKeyPoints(self, featuresKeys, check_first, counter): # todo needs to add matcher object
-
-        if matcher_object == BF # todo needs to add BF and KNN
-            rawmatch = self.__matchKeyPointsBF(featuresKeys, )
 
 
 
-    def matchKeyPointsBF(self, featuresKeys, check_first, counter):
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def matchKeyPointsBF(self): # that's the old one, delete later
         bf = createMatcher(crossCheck=True)
         # Match descriptors.
-
+b
         index_a = check_first
         index = 0
         best_matches[[]] # todo - is that ok?
